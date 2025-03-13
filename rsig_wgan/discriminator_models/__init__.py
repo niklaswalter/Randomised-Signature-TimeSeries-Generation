125b87e0682491424bdf5544add789bfd251b507
Private User <privateuser@Niklass-MacBook-Pro.local>
2025-03-13 20:32:56 +0100

    refactoring of rsig discriminator

---
+++ rsig_wgan/discriminator_models/__init__.py
@@ -0,0 +1,4 @@
+from .utils import l2_dist
+from .rsigw1 import RSigWGANTraining
+
+__all__ = ["l2_dist", "RSigWGANTraining"]
---
+++ rsig_wgan/discriminator_models/utils.py
@@ -0,0 +1,2 @@
+def l2_dist(x, y: float) -> float:
+    return (x - y).pow(2).sum().sqrt()
